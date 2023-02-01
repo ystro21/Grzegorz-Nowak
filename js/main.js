e3fbef35a8a7cf0958175f1242edc26f0de08f12
Grzegorz <ystro21@wp.pl>
2023-02-01 16:26:30 +0100

    Add portfolio english

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 const menu = document.querySelector('.menu-icon')
 const navbar = document.querySelector('.navbar')
 
+
 menu.addEventListener("click", () =>{
     menu.classList.toggle("move")
     navbar.classList.toggle("open-menu")
